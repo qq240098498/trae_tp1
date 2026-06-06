81e56d0c2d492be5230606daf173f7c908ca8e51
qq240098498 <240098498@qq.com>
2026-06-06 23:33:10 +0800

    .1523281629553499:3d6bc0c6ce381d51a71a9a7ff93c510e_6a2431ab641c38656d9a9551.6a243ab8641c38656d9a9617.6a243ab77fca55049db30490:Trae CN.T(2026/6/6 23:20:24)

--- src/main/java/com/huolala/controller/IndexController.java
+++ src/main/java/com/huolala/controller/IndexController.java
@@ -1,10 +1,15 @@
 package com.huolala.controller;
 
+import com.huolala.dto.DriverMileageStats;
 import com.huolala.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -26,6 +31,22 @@ public class IndexController {
         model.addAttribute("orderCount", orderService.findAll().size());
         model.addAttribute("freightCount", freightConfigService.findAll().size());
         model.addAttribute("pendingOrderCount", orderService.findByStatus(0).size());
+        
+        String currentMonth = YearMonth.now().format(DateTimeFormatter.ofPattern("yyyy-MM"));
+        List<DriverMileageStats> monthStats = orderService.getDriverMileageStats(currentMonth, "totalMileage", "desc");
+        
+        double totalMileage = monthStats.stream()
+                .mapToDouble(DriverMileageStats::getTotalMileage)
+                .sum();
+        int monthOrderCount = monthStats.stream()
+                .mapToInt(DriverMileageStats::getOrderCount)
+                .sum();
+        
+        model.addAttribute("monthTotalMileage", Math.round(totalMileage * 100.0) / 100.0);
+        model.addAttribute("monthOrderCount", monthOrderCount);
+        model.addAttribute("currentMonth", currentMonth);
+        model.addAttribute("topDrivers", monthStats.size() > 3 ? monthStats.subList(0, 3) : monthStats);
+        
         return "index";
     }
 }
